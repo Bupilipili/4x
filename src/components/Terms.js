b7bd0f1b0227c10910b90303f5fb4a1957d90b93
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-04 21:01:27 +0300

    Create Terms.js

---
+++ src/components/Terms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Terms() {
+  return (
+    <div className="section">
+      <h1>Terms Page</h1>
+      {/* Add content related to the store */}
+    </div>
+  );
+}
+
+export default Terms;
