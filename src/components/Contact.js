94c2f446bf7c2d97395a77df711d3772b46f4c39
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-04 21:03:50 +0300

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+// components/Contact.js
+import React from 'react';
+import './styles/Contact.css';
+
+function Contact() {
+  return (
+    <div className="section">
+      <h1>Contact Us</h1>
+      {/* Add content related to contact */}
+    </div>
+  );
+}
+
+export default Contact;
