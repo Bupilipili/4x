7b73ed1e15025071bef08718245d794b4f4e166d
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-04 21:03:42 +0300

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Home.css';
+
+function Home() {
+  return (
+    <div className="section">
+      <h1>Welcome to 4le Studios</h1>
+      <p>Your hub for awesome games and services.</p>
+    </div>
+  );
+}
+
+export default Home;
