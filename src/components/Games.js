3c6a81cbe1bb0fd89f7e6050c398bf7260fe3da0
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-04 21:03:46 +0300

    Create Games.js

---
+++ src/components/Games.js
@@ -0,0 +1,14 @@
+// components/Games.js
+import React from 'react';
+import './styles/Games.css';
+
+function Games() {
+  return (
+    <div className="section">
+      <h1>Our Exciting Games</h1>
+      {/* Add content related to games */}
+    </div>
+  );
+}
+
+export default Games;
