739ec75a27ed8e7ab834f957c6e7f63a3fe75e78
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-04 21:03:58 +0300

    Create Careers.js

---
+++ src/components/Careers.js
@@ -0,0 +1,13 @@
+// components/Store.js
+import React from 'react';
+
+function Careers() {
+  return (
+    <div className="section">
+      <h1>Careers Page</h1>
+      {/* Add content related to the store */}
+    </div>
+  );
+}
+
+export default Careers;
