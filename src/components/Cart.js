f162ed1ab9a3857eff01b2983d90149453eb545e
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-04 21:03:54 +0300

    Create Cart.js

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+// components/Store.js
+import React from 'react';
+
+function Cart() {
+  return (
+    <div className="section">
+      <h1>Cart Page</h1>
+      {/* Add content related to the store */}
+    </div>
+  );
+}
+
+export default Cart;
