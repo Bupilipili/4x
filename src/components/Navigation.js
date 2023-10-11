b0d1e67ad78205d74cc5834b0f5356ea3a8faeee
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-11 12:41:40 +0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa'; // Hamburger icon from react-icons
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './styles/Navigation.css';
 
 function Navigation() {
@@ -10,42 +10,46 @@ function Navigation() {
     setShowMenu(!showMenu);
   };
 
+  const handleLinkClick = () => {
+    setShowMenu(false); // Hide the navbar when a link is clicked
+  };
+
   return (
     <nav className="navbar">
-    <h3>4le Studios</h3>
-    <div className="menu-toggle" onClick={handleToggleMenu}>
-      <FaBars />
-    </div>
-    <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
-     <div className="cancel-toggle" onClick={handleToggleMenu}>
-     <FaTimes />
-       </div> 
-        <li>
+      <h3>4le Studios</h3>
+      <div className="menu-toggle" onClick={handleToggleMenu}>
+        <FaBars />
+      </div>
+      <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
+        <div className="cancel-toggle" onClick={handleToggleMenu}>
+          <FaTimes />
+        </div>
+        <li onClick={handleLinkClick}>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li onClick={handleLinkClick}>
           <Link to="/games">Games</Link>
         </li>
-        <li>
+        <li onClick={handleLinkClick}>
           <Link to="/services">Services</Link>
         </li>
-        <li>
+        <li onClick={handleLinkClick}>
           <Link to="/about">About</Link>
         </li>
         <li className="dropdown">
           <span>Pages</span>
           <ul className="dropdown-menu">
-            <li><Link to="/pages/store">Store</Link></li>
-            <li><Link to="/pages/cart">Cart</Link></li>
-            <li><Link to="/pages/careers">Careers</Link></li>
-            <li><Link to="/pages/privacy-policy">Privacy Policy</Link></li>
-            <li><Link to="/pages/terms-and-conditions">Terms and Conditions</Link></li>
+            <li onClick={handleLinkClick}><Link to="/pages/store">Store</Link></li>
+            <li onClick={handleLinkClick}><Link to="/pages/cart">Cart</Link></li>
+            <li onClick={handleLinkClick}><Link to="/pages/careers">Careers</Link></li>
+            <li onClick={handleLinkClick}><Link to="/pages/privacy-policy">Privacy Policy</Link></li>
+            <li onClick={handleLinkClick}><Link to="/pages/terms-and-conditions">Terms and Conditions</Link></li>
           </ul>
         </li>
-        <li>
+        <li onClick={handleLinkClick}>
           <Link to="/blog">Blog</Link>
         </li>
-        <li>
+        <li onClick={handleLinkClick}>
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
