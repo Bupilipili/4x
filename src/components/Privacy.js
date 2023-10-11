9f313a0b6279e52465836694727f17b1491b41c0
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-12 01:52:10 +0300

    Update Privacy.js

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Privacy() {
   return (
-    <div className="section">
+    <div>
       <h1>Privacy Page</h1>
       {/* Add content related to the store */}
     </div>
