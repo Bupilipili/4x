0fedc7ee883bd1883c8892d9f9282dbb3b7b6c84
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-12 01:52:33 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/Home.css';
 
+const words = ['Entertainment', 'Pleasure', 'Enjoyment'];
+
 function Home() {
+  const [currentWord, setCurrentWord] = useState('Entertainment');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWord(oldWord => {
+        const index = words.indexOf(oldWord);
+        return words[(index + 1) % words.length];
+      });
+    }, 3000); // Change word every 3 seconds
+
+    return () => clearInterval(interval); // Clean up on component unmount
+  }, []);
+
   return (
-    <div className="section">
-      <h1>Welcome to 4le Studios</h1>
-      <p>Your hub for awesome games and services.</p>
     <div>
-      We build mobile gaming worlds
-    </div>
+      <h2 className='head2'>Building Mobile Gaming Worlds</h2>
+      <h1 className='head1'>We craft games <br/> 
+        <span className='fade'>{currentWord}</span>
+      </h1>
     </div>
   );
 }
