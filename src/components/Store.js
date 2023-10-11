268577b2e3208d571469e4775da68e6adc7ca9fb
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-12 01:52:23 +0300

    Update Store.js

--- src/components/Store.js
+++ src/components/Store.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Store() {
   return (
-    <div className="section">
+    <div>
       <h1>Store Page</h1>
       {/* Add content related to the store */}
     </div>
