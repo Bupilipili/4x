67ca71e6f4ce0b3629011851ba00071c28c0eb4e
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-16 12:52:56 +0300

    Create Privacy.js

---
+++ src/components/js/Privacy.js
@@ -0,0 +1,13 @@
+// components/Store.js
+import React from 'react';
+
+function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Page</h1>
+      {/* Add content related to the store */}
+    </div>
+  );
+}
+
+export default Privacy;
