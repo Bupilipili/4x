67baf3e0cdc65559f3210e166900e3b7e09c8d4b
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-16 12:52:15 +0300

    Create About.js

---
+++ src/components/js/About.js
@@ -0,0 +1,14 @@
+// components/About.js
+import React from 'react';
+import '../styles/About.css';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      {/* Add content related to about us */}
+    </div>
+  );
+}
+
+export default About;
