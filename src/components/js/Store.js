73289752d0da3c7cbdf151a736b2c8b236f7f672
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-16 12:53:05 +0300

    Create Store.js

---
+++ src/components/js/Store.js
@@ -0,0 +1,13 @@
+// components/Store.js
+import React from 'react';
+
+function Store() {
+  return (
+    <div>
+      <h1>Store Page</h1>
+      {/* Add content related to the store */}
+    </div>
+  );
+}
+
+export default Store;
