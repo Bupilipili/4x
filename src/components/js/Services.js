e97bdb1892a9dd61a488c2e755ec2fee7e0878b0
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-16 12:53:01 +0300

    Create Services.js

---
+++ src/components/js/Services.js
@@ -0,0 +1,14 @@
+// components/Services.js
+import React from 'react';
+import '../styles/Services.css';
+
+function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      {/* Add content related to services */}
+    </div>
+  );
+}
+
+export default Services;
